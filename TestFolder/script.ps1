489ed7993e52dcfbd73708e5c4fccde1d8483d8c
edhurt7 <edward.hurtado1@outlook.com>
2024-03-12 09:41:41 -0700

    update

--- TestFolder/script.ps1
+++
@@ -1,7 +0,0 @@
-
-#Gets a list of Plug-n-Play devices with a status of OK
-
-Get-PnpDevice -PresentOnly |Where-Object {$_.Status -eq 'OK'} | ft -Property Status,FriendlyName,Manufacturer,PSComputerName | Out-File C:\report.txt
-
-
-
