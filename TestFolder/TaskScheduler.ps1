1ec3f3dc71181ae4a938fc6a6f343f0bcae98acd
edhurt7 <edward.hurtado1@outlook.com>
2024-03-14 08:12:03 -0700

    update

---
+++ TestFolder/TaskScheduler.ps1
@@ -0,0 +1,14 @@
+
+# Define the action to execute the PowerShell script
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File C:\usbdevices.ps1"
+
+# Create a trigger for daily execution
+$trigger = New-ScheduledTaskTrigger -Daily -At "3:39pm"
+
+# Register the scheduled task
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MyDailyTask5" -Description "This task runs usbdevices.ps1 daily at 3:39pm"
+
+#Start the Task
+Start-ScheduledTask -TaskName "MyDailyTask5"
+
+
